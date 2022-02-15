799c594c36d8ca72743c638d47f8972c532b3d7c
shalinshah54-lab <shalinshah54@hotmail.com>
2022-02-15 21:48:29 +0000

    added the stripe to the project for safe and secure use of payment options.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KTYTDC0sK9zZ5Bc99MLoWAsoNkemlWQfFKhNinJoayQLMXiIC3TP5GbiXRHgkiMFdwhfIGZc4rqHcEYEMEA6zJI00IUhUSwyB',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
