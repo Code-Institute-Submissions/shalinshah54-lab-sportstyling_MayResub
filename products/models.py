df09f01c3f44ca39f34aea807dab017590155731
shalinshah54-lab <shalinshah54@hotmail.com>
2022-02-16 17:14:31 +0000

    added the stripe using the public key and secret key and added the js functionality.

--- products/models.py
+++ products/models.py
@@ -9,11 +9,13 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-    def __str__(self):
-        return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
+def __str__(self):
+    return self.name
+
+
+def get_friendly_name(self):
+    return self.friendly_name
 
 
 class Product(models.Model):
