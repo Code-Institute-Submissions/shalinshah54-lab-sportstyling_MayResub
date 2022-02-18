7d3fde1ba1173033b049941ee34ef59e6f000088
shalinshah54-lab <shalinshah54@hotmail.com>
2022-02-18 18:29:14 +0000

    added the wrong file to the version control.

--- sportstyling/settings.py
+++ sportstyling/settings.py
@@ -126,21 +126,18 @@ WSGI_APPLICATION = 'sportstyling.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://bwhsuirhwzwvqc:0d5496f77ab799ccce7c5eea66866e82c30ef99c69453cf0c726aab2bae91e86@ec2-52-204-196-4.compute-1.amazonaws.com:5432/d481fq8mkcmi77')
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
