0d3a7dd9cb563783088b41d6599c1c3d1e5c2d5e
shalinshah54-lab <shalinshah54@hotmail.com>
2022-02-18 19:35:28 +0000

    deployed the site to heroku and added the secret key and debug.

--- sportstyling/settings.py
+++ sportstyling/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2q7g@^quhsrv7gcsiidyb9xswlvm0zg6ld6&mhgy0s0^o#qflk'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['shalinshah54-lab-sportstyling.herokuapp.com', 'localhost']
 
