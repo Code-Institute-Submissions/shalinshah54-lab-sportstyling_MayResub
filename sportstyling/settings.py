e6754787e771ca8b6eb7eb7e78f26b0b20e011b8
shalinshah54-lab <shalinshah54@hotmail.com>
2022-02-18 17:49:17 +0000

    working on deploying to heuroku some requirements have been added and installed.

--- sportstyling/settings.py
+++ sportstyling/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,12 +125,6 @@ WSGI_APPLICATION = 'sportstyling.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
